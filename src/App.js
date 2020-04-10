97507bcc498828e44f26fb8b6f142a95664708da
Fabiano Vilela <fabiano.vilela@gmail.com>
2020-04-10 18:48:09 -0300

    Next level

--- src/App.js
+++ src/App.js
@@ -1,33 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { FiTrash2 } from "react-icons/fi";
 
+import api from "./services/api";
 import "./App.css";
-import backgroundImage from "./assets/background.png";
 
 import Header from "./Components/Header";
 
 function App() {
-  const [projects, setProject] = useState([
-    "Desenvolvimento de app",
-    "Fron-end web",
-  ]);
+  const [projects, setProject] = useState([]);
+  const [title, setTitle] = useState("");
+  const [owner, setOwner] = useState("");
 
-  function handleAddProject() {
-    setProject([...projects, `Novo projeto ${Date.now()}`]);
+  useEffect(() => {
+    api.get("/projects").then((response) => {
+      setProject(response.data);
+    });
+  }, []);
+
+  async function handleAddProject(e) {
+    e.preventDefault();
+
+    const data = {
+      title,
+      owner,
+    };
+
+    const response = await api.post("/projects", data);
+
+    setProject([...projects, response.data]);
+    setTitle("");
+    setOwner("");
+  }
+
+  async function handleDeleteProject(id) {
+    try {
+      await api.delete(`/projects/${id}`);
+
+      setProject(projects.filter((project) => project.id !== id));
+    } catch (err) {
+      alert("Erro ao deletar o projeto");
+    }
   }
 
   return (
-    <>
-      <Header title='Projectos' />
-      <img width={400} src={backgroundImage} />
+    <div className='project-container'>
+      <Header title='Projetos' />
+
+      <form onSubmit={handleAddProject}>
+        <input
+          placeholder='Título do projeto'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <input
+          placeholder='Proprietário'
+          value={owner}
+          onChange={(e) => setOwner(e.target.value)}
+        />
+
+        <button type='submit' className='button'>
+          Adicionar projeto
+        </button>
+      </form>
+
       <ul>
         {projects.map((project) => (
-          <li key={project}>{project}</li>
+          <li key={project.id}>
+            <strong>PROJETO: </strong>
+            {project.title}
+            <p>
+              <strong>PROPRIETÁRIO: </strong>
+              {project.owner}
+            </p>
+            <button
+              type='button'
+              onClick={() => handleDeleteProject(project.id)}
+            >
+              <FiTrash2 />
+            </button>
+          </li>
         ))}
       </ul>
-      <button type='button' onClick={() => handleAddProject()}>
-        Adicionar projeto
-      </button>
-    </>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 
 import App from "./App";
-import Header from "./Components/Header";
+// import Header from "./Components/Header";
 
 render(
   <>
